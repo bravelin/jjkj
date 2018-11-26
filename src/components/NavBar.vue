2dfa6c5f0e10db0b963b0ae3f6fb3305ff0b6424
zhangjialin <zhangjialin@51jjkj.com>
2018-11-26 18:06:20 +0800

    add stars

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -5,8 +5,9 @@
             <ul class="nav-list">
                 <li>首页</li>
                 <li>我们的服务</li>
-                <li>平台优势</li>
                 <li>合作客户</li>
+                <li>平台优势</li>
+                <li>关于我们</li>
                 <li>联系我们</li>
             </ul>
         </div>
--- src/views/s1/Sphere.vue
+++ src/views/s1/Sphere.vue
@@ -3,14 +3,16 @@
 </template>
 <script>
     import * as THREE from 'three'
+    let scene = null
+    let camera = null
+    let renderer = null
+    let sphere = null
+    let container = null
+    let starts = null
+    let light = null
     export default {
         data () {
             return {
-                scene: null,
-                camera: null,
-                renderer: null,
-                sphere: null,
-                container: null,
                 width: 0,
                 height: 0,
                 requestId: 0
@@ -27,35 +29,102 @@
             init () {
                 const that = this
                 const win = window
-                that.container = that.$el
-                const scene = that.scene = new THREE.Scene()
+                container = that.$el
+                scene = new THREE.Scene()
                 that.width = win.innerWidth
                 that.height = win.innerHeight
 
-                const camera = that.camera = new THREE.PerspectiveCamera(45, that.width / that.height, 0.1, 10000)
+                camera = new THREE.PerspectiveCamera(45, that.width / that.height, 0.1, 10000)
                 camera.position.x = -180
                 camera.position.y = 0
                 camera.position.z = 500
                 camera.lookAt(scene.position)
 
-                const renderer = that.renderer = new THREE.WebGLRenderer({ antialias: true })
+                renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true })
                 renderer.setClearColor(0x202276)
                 renderer.setSize(that.width, that.height)
-                that.container.appendChild(renderer.domElement)
+                renderer.shadowMap.enabled = true
+                renderer.shadowMap.type = THREE.PCFSoftShadowMap
+                container.appendChild(renderer.domElement)
 
-                const geometry = new THREE.IcosahedronGeometry(100, 4)
-                const sphereBuffer = new THREE.BufferGeometry().fromGeometry(geometry)
-                const material = new THREE.MeshNormalMaterial({
-                    color: 0xffffff,
-                    opacity: 0.5,
-                    transparent: true
+                that.drawStar() // 背景星星
+                that.drawLight() // 添加球中心的光晕
+                that.drawSphere() // 添加旋转的球
+            },
+            drawLight () {
+                const canvas = document.createElement('canvas')
+                canvas.width = 30
+                canvas.height = 30
+                const color = '196,233,255'
+                const context = canvas.getContext('2d')
+                const gradient = context.createRadialGradient(canvas.width / 2, canvas.height / 2, 0, canvas.width / 2, canvas.height / 2, canvas.width / 2)
+                gradient.addColorStop(0, 'rgba(' + color + ',1)')
+                gradient.addColorStop(0.2, 'rgba(' + color + ',.7)')
+                gradient.addColorStop(0.4, 'rgba(' + color + ',.5)')
+                gradient.addColorStop(1, 'rgba(0,0,0,0)')
+                context.fillStyle = gradient
+                context.fillRect(0, 0, canvas.width, canvas.height)
+
+                const that = this
+                const size = 180
+                const material = new THREE.SpriteMaterial({
+                    map: new THREE.CanvasTexture(canvas), blending: THREE.AdditiveBlending
                 })
-                const sphere = that.sphere = new THREE.Mesh(sphereBuffer, material)
+                const geometry = new THREE.CircleGeometry(size, 50)
+                const mesh = new THREE.Sprite(material)
+                mesh.scale.x = mesh.scale.y = size * 1.5
+                mesh.position.z = 0
+                mesh.position.x = -180
+                mesh.position.y = 0
+                scene.add(mesh)
+            },
+            generateSprite () {
+                const canvas = document.createElement('canvas')
+                canvas.width = 16
+                canvas.height = 16
+                const color = '196,233,255'
+                const context = canvas.getContext('2d')
+                const gradient = context.createRadialGradient(canvas.width / 2, canvas.height / 2, 0, canvas.width / 2, canvas.height / 2, canvas.width / 2)
+                gradient.addColorStop(0, 'rgba(' + color + ',1)')
+                gradient.addColorStop(0.2, 'rgba(' + color + ',1)')
+                gradient.addColorStop(0.4, 'rgba(' + color + ',.5)')
+                gradient.addColorStop(1, 'rgba(0,0,0,0)')
+                context.fillStyle = gradient
+                context.fillRect(0, 0, canvas.width, canvas.height)
+                return canvas
+            },
+            drawStar () { // 星星
+                const that = this
+                const starSize = 1.6
+                const material = new THREE.SpriteMaterial({
+                    map: new THREE.CanvasTexture(that.generateSprite()),
+                    blending: THREE.AdditiveBlending
+                })
+                const geometry = new THREE.CircleGeometry(starSize, 50)
+                const starNum = 500
+                let mesh = null
+                starts = new THREE.Object3D()
+                for (let i = 0; i < starNum; i++) {
+                    mesh = new THREE.Sprite(material)
+                    mesh.scale.x = mesh.scale.y = starSize * 1.5
+                    mesh.position.z = -400 + Math.random() * 700
+                    mesh.position.x = -350 + Math.random() * 700
+                    mesh.position.y = -300 + Math.random() * 600
+                    starts.add(mesh)
+                }
+                scene.add(starts)
+                starts.position.set(-180, 0, 0)
+            },
+            drawSphere () { // 球
+                const geometry = new THREE.IcosahedronGeometry(100, 2)
+                const sphereBuffer = new THREE.BufferGeometry().fromGeometry(geometry)
+                const material = new THREE.MeshLambertMaterial({ color: 0x192376, transparent: true, opacity: 0.2 })
+                sphere = new THREE.Mesh(sphereBuffer, material)
                 scene.add(sphere)
 
                 const edgesGeometry = new THREE.EdgesGeometry(sphere.geometry)
                 const edgesMaterial = new THREE.LineDashedMaterial({
-                    color: 0xffffff,
+                    color: 0xe0e0e0,
                     dashSize: 0.5,
                     gapSize: 0.5
                 })
@@ -63,11 +132,14 @@
                 edges.computeLineDistances()
                 sphere.add(edges)
                 sphere.translateX(-180)
+                sphere.castShadow = true
             },
             animate () {
                 const that = this
-                that.sphere.rotation.y += 0.005
-                that.renderer.render(that.scene, that.camera)
+                const time = Date.now() * 0.0005
+                sphere.rotation.y += 0.002
+                starts.rotation.y += 0.0001
+                renderer.render(scene, camera)
                 that.requestId = requestAnimationFrame(that.animate)
             }
         },
