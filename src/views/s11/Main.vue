99c6fe87d52fa4ae5a3403047bcc7dd0a18bff72
bravelin <jialin2013@foxmail.com>
2018-11-29 21:54:28 +0800

    新的首屏

--- src/views/s11/Main.vue
+++ src/views/s11/Main.vue
@@ -3,9 +3,9 @@
         <div class="text-zone">
             <Arrow class="ani" swiper-animate-effect="fadeInUp" swiper-animate-duration="1.2s" swiper-animate-delay="0.3s"></Arrow>
             <div class="text">
-                <div>BLOCK CHACHAINPAYMENT</div>
-                <div>区块链</div>
-                <div>安全 可靠 跨平台</div>
+                <div class="ani" swiper-animate-effect="fadeInLeft" swiper-animate-duration="1s" swiper-animate-delay="1.1s">BLOCK CHACHAINPAYMENT</div>
+                <div class="ani" swiper-animate-effect="fadeInDown" swiper-animate-duration="1.2s" swiper-animate-delay="0.3s">区块链</div>
+                <div class="ani" swiper-animate-effect="fadeInRight" swiper-animate-duration="1s" swiper-animate-delay="1.1s">安全 可靠 跨平台</div>
             </div>
         </div>
     </div>
